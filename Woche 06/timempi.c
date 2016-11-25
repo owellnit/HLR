b7082a4e290f72a479e1ed72b94e4678482d98a8
Ole Wellnitz <olewellnitz@Oles-MacBook-Pro.local>
2016-11-25 20:32:47 +0100

    auch 1

--- Woche 06/timempi.c
+++ Woche 06/timempi.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <limits.h>
 
 const int HOSTNAME_LENGTH = 40;
 const int TIMESTAMP_LENGTH = 40;
@@ -10,43 +11,56 @@ const int BUFFER_SIZE = 80;
 
 int main(int argc, char **argv)
 {
-  int numberOfProcesses, actualProcess;
-  MPI_Status status;
-
-  MPI_Init(&argc, &argv);
-  MPI_Comm_rank(MPI_COMM_WORLD, &actualProcess);
-  MPI_Comm_size(MPI_COMM_WORLD, &numberOfProcesses);
-
-  if (actualProcess == 0)
-  {
-  	char buffer[BUFFER_SIZE];
-  	for (int i = 1; i < numberOfProcesses; i++)
-  	{
-  		MPI_Recv(buffer, BUFFER_SIZE, MPI_CHAR, i, 0, MPI_COMM_WORLD, &status);
-  		printf("%s\n", buffer); 
-  	}
-  }
-  else
-  {
-  	char hostname[HOSTNAME_LENGTH];
-    	char timestamp[TIMESTAMP_LENGTH];
-    	char buffer[BUFFER_SIZE];
-  	struct timeval time;
-	time_t nowtime;
-
-  	gethostname(hostname, HOSTNAME_LENGTH);
-    	gettimeofday(&time, NULL);
+    //Anzahl der gesamt Prozesse und der aktuelle Prozess
+    int numberOfProcesses, actualProcess;
+    MPI_Status status;
     
-	nowtime = time.tv_sec;
-    	strftime(timestamp, sizeof(timestamp), "%F %T", localtime(&nowtime));
-      
-    	sprintf(buffer, "%s: %s.%06ld", hostname, timestamp, time.tv_usec);
+    //MPI initialisieren
+    MPI_Init(&argc, &argv);
+    //Die Nummer des aktuellen Prozesses ermitteln
+    MPI_Comm_rank(MPI_COMM_WORLD, &actualProcess);
     
-    	MPI_Send(buffer, BUFFER_SIZE, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
-  }
-
-  MPI_Barrier(MPI_COMM_WORLD);
-
-  printf("Rang %d beendet jetzt!\n", actualProcess);
-  MPI_Finalize();
+    //Die Anzahl aller Prozesse ermitteln
+    MPI_Comm_size(MPI_COMM_WORLD, &numberOfProcesses);
+    
+    //Wenn der aktuelle Prozess die Nummer 0 hat, in den Lesemodus
+    if (actualProcess == 0)
+    {
+        //Nachrichten der anderen Threads der Reihenfolge nach auslesen
+        char buffer[BUFFER_SIZE];
+        for (int i = 1; i < numberOfProcesses; i++)
+        {
+            MPI_Recv(buffer, BUFFER_SIZE, MPI_CHAR, i, 0, MPI_COMM_WORLD, &status);
+            printf("%s\n", buffer);
+        }
+    }
+    else
+    {
+        char hostname[HOSTNAME_LENGTH];
+        char timestamp[TIMESTAMP_LENGTH];
+        char buffer[BUFFER_SIZE];
+        struct timeval time;
+        time_t nowtime;
+        
+        //Hostname und Time ermitteln
+        gethostname(hostname, HOSTNAME_LENGTH);
+        gettimeofday(&time, NULL);
+        
+        //Time in String wandeln
+        nowtime = time.tv_sec;
+        strftime(timestamp, sizeof(timestamp), "%F %T", localtime(&nowtime));
+        
+        //Ausgabe mit Hostname und Timestamp zusammenbauen
+        sprintf(buffer, "%s: %s.%06ld", hostname, timestamp, time.tv_usec);
+        
+        //Daten über MPI senden
+        MPI_Send(buffer, BUFFER_SIZE, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+    }
+    
+    //'Wartepunkt' für die Threads
+    MPI_Barrier(MPI_COMM_WORLD);
+    
+    //Melden, dass der THread fertig ist
+    printf("Rang %d beendet jetzt!\n", actualProcess);
+    MPI_Finalize();
 }

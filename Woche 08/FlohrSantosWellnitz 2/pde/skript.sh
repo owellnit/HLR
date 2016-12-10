a6f9c84dcb1e5fb96321bcd27ba7af97a1c1dbe5
Ole Wellnitz <OleWellnitz@outlook.com>
2016-12-11 00:21:45 +0100

    test

---
+++ Woche 08/FlohrSantosWellnitz 2/pde/skript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Output in "job.out", time and erros in "job.err"
+#SBATCH --output=skript.out
+#SBATCH --error=skript.err
+#SBATCH -p west
+#SBATCH -N 2
+#SBATCH --ntasks-per-node=12
+
+. /etc/profile.d/wr-spack.sh
+spack load --dependencies mpi
+
+if [ "${SLURM_PARTITION}" != 'abu' ]
+then
+export MPICH_NEMESIS_NETMOD=tcp
+fi
+
+mpirun ./partdiff-par 1 2 512 2 2 2048
